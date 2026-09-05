09dffe769eaa3a882de07a207346de1a2569258f
Param Factory <factory@sociobot.in>
2026-09-05 21:00:56 +0000

    perf: eliminate first-load layout shift

--- site/src/shell.ts
+++ site/src/shell.ts
@@ -1,6 +1,3 @@
-import "@fontsource/inter/latin-400.css";
-import "@fontsource/inter/latin-600.css";
-import "@fontsource/ibm-plex-mono/latin-500.css";
 import "./styles.css";
 
 const path = window.location.pathname;
